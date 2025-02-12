d7b878fa853cd34d3137816b1577028afc5705de
Dominic German <dominicgerman@gmail.com>
2025-02-12 09:48:19 -0600

    Deploy script

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o /usr/local/bin/portfolio ./cmd/web/
+CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o "$BUILD_PATH" ./cmd/web/
+
+sudo mv "$BUILD_PATH" /usr/local/bin/portfolio
---
+++ scripts/deployprod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull origin main
+.scripts/buildprod.sh
+sudo systemctl restart portfolio.service
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
+     -H "Authorization: Bearer $CLOUDFLARE_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
