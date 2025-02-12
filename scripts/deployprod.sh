9ef3ba1f5f93ebd622221de60b05f27e2dbb5680
Dominic German <dominicgerman@gmail.com>
2025-02-12 11:50:14 -0600

    Updated README

--- scripts/deployprod.sh
+++ scripts/deployprod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull origin main
-.scripts/buildprod.sh
+./scripts/buildprod.sh
 sudo systemctl restart portfolio.service
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
      -H "Authorization: Bearer $CLOUDFLARE_TOKEN" \
